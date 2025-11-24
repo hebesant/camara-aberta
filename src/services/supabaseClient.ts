76b034ec6100e44d958f8da26ba39ded50da57d2
Heberty dos Santos <hebertysant@gmail.com>
2025-11-24 09:55:14 -0300

    update supabase auth

--- src/services/supabaseClient.ts
+++ src/services/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY!
+const supabaseUrl = "https://loahjrgofgbeiyvfhmfh.supabase.co"
+const supabaseAnonKey = "sb_publishable_h8Ti0DuistPeHTAxSEy9CQ_BGvH3NCm"
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error("As variáveis de ambiente do Supabase não foram definidas.");
